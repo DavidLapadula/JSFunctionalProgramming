2c9ffd0eeb2d4838eef6d718d3fe304474f119ca
DavidLapadula <lapadula.david@gmail.com>
2022-06-14 22:22:54 -0400

    [composition] created compose and pipe HOF's

--- Composition/index.js
+++ Composition/index.js
@@ -12,3 +12,59 @@ var sumArrow = (num1, num2) => num1 + num2;
 var square = num => num * num;
 // need parentheses with no args
 var oneHundred = () => 100;
+
+str = 'Innovation distinguishes between a leader and a follower.';
+let prepareString = function() {
+    let arr = str.trim().replace(/[?.,!]/g,'').toUpperCase().split(" "); // all these items can be put into their own function. Will make them re-usable
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'A' || arr[i] === 'AN' || arr[i] === 'THE') {
+            arr.splice(i,1); // remove articles
+        }
+    }
+    return arr;
+};
+
+// Ex. Arrow functions used in FP and defined with 'const' to prevent mutation
+const trim = str => str.replace(/^\s*|\s*$/g, '');
+
+const noPunct = str => str.replace(/[?.,!]/g,'');
+
+const capitalize = str => str.toUpperCase();
+
+const breakout = str => str.split(" ");
+
+const noArticles = str => (str !== "A" && str !== "AN" && str !== "THE");
+
+const filterArticles = arr => arr.filter(noArticles);
+
+// Called right to left, or last called to first called
+const compose = function(...fns) {
+    return function(x) {
+        // call function on value and reduce it to a single value. x becomes first value and then used as accumulated value for next iterations
+        return fns.reduceRight(function(v, f) {
+            return f(v);
+        }, x);
+    }
+};
+
+// same as reduce except called left to right, or first called to last called
+const pipe = function(...fns) {
+    return function(x) {
+        return fns.reduce(function(v, f) {
+            return f(v);
+        }, x);
+    }
+};
+
+// compose returns a fn that is then called usins prepareString wrapper
+const prepareStringComp = compose(
+    filterArticles,
+    breakout,
+    capitalize,
+    noPunct,
+    trim
+); 
+
+prepareString(str);
+
