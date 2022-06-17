b00b94579964275f39627ed22173035a4f1b7e8d
DavidLapadula <lapadula.david@gmail.com>
2022-06-16 22:03:01 -0400

    [composition] begin testing functions for currying

--- Composition/index.js
+++ Composition/index.js
@@ -68,3 +68,53 @@ const prepareStringComp = compose(
 
 prepareString(str);
 
+// Exercise 6
+const scores = [50, 6, 100, 0, 10, 75, 8, 60, 90, 80, 0, 30, 110];
+const boostSingleScores = scores.map(val => (val < 10) ? val * 10 : val);
+const rmvOverScores = boostSingleScores.filter(val => val <= 100);
+const rmvZeroScores = rmvOverScores.filter(val => val > 0);
+const scoresSum = rmvZeroScores.reduce((sum, val) => sum + val, 0);
+const scoresCnt = rmvZeroScores.reduce((cnt, val) => cnt + 1, 0);
+
+//Convert each statement to a function that can accept and act on any array.
+const boostSingleScoresGeneric = items => items.map(val => (val < 10) ? val * 10 : val);
+const rmvOverScoresGeneric = items => items.filter(val => val <= 100);
+const rmvZeroScoresGeneric = items => items.filter(val => val > 0);
+const scoresSumGeneric = items => items.reduce((sum, val) => sum + val, 0);
+const scoresCntGeneric = items => items.reduce((cnt, val) => cnt + 1, 0);
+
+//Compose a function that will remove both zero or lower scores and scores over 100. Test it using the scores array.
+const noZeroOrBelowOrOverOneHundred = pipe(
+    rmvOverScoresGeneric,
+    rmvZeroScoresGeneric
+); 
+// Need to call this with array as parameter because pipe/compose return a function
+
+//Compose a function that will do all the modifications to an array. Test it using the scores array.
+const allModifications = pipe(
+    boostSingleScoresGeneric,
+    rmvOverScoresGeneric,
+    rmvZeroScoresGeneric,
+    scoresSumGeneric,
+    scoresCntGeneric
+); 
+
+//Create a function that will accept an array and return the average. Use the function that sums scores and the function that counts scores or the length property.
+const average = items => Math.round(scoresSumGeneric(items) / items.length);
+
+// Arity (# of parameters a fn has) - pipe and compose limited by functions taking 1 argument or Airty of 1
+// bind() allows you to partially apply a parameter and 'bind' the value to a new function
+
+// Currying
+const curryGreeting = (greeting) => {
+    // this function creates a closure for greeting
+    return function(name) {
+        console.log(greeting + ' ' + name);
+    }
+}; 
+
+const welcomeGreet = curryGreeting("Welcome");
+welcomeGreet("Steve");
+
+
+
