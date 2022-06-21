ef9f49ce2ea4798b68aabb380766568b4b6a826a
DavidLapadula <lapadula.david@gmail.com>
2022-06-20 22:21:39 -0400

    [composition] use currying for function composition

--- Composition/index.js
+++ Composition/index.js
@@ -105,7 +105,8 @@ const average = items => Math.round(scoresSumGeneric(items) / items.length);
 // Arity (# of parameters a fn has) - pipe and compose limited by functions taking 1 argument or Airty of 1
 // bind() allows you to partially apply a parameter and 'bind' the value to a new function
 
-// Currying
+// Currying - reduce high arity functions for compositions that require one argument
+// Can also curry to save one form on a function with a fixed  value
 const curryGreeting = (greeting) => {
     // this function creates a closure for greeting
     return function(name) {
@@ -116,5 +117,97 @@ const curryGreeting = (greeting) => {
 const welcomeGreet = curryGreeting("Welcome");
 welcomeGreet("Steve");
 
+// Length of fn is number of arguments it can accept, and FIRST argument needs to be a function
+function curry(fn,arity = fn.length) {
+    return (function nextCurried(prevArgs){
+        // Closure created here gives access to values passed in with multiple calls to function
+        return function curried(nextArg){
+            var args = [ ...prevArgs, nextArg ];
+            // ony call function when it has the right number of arguments
+            if (args.length >= arity) {
+                return fn( ...args );
+            }
+            else {
+                return nextCurried( args );
+            }
+        };
+    })( [] );
+}
 
+const ffun = (a, b, c) => {
+    return a + b + c;
+}
+
+const curriedF = curry(ffun);
+
+// each argument result passed to the next one, so need to be aware of arguments needed
+const newFun = pipe(
+    curriedF(1)(2)
+);
+
+// Partial application: function applied to some but not all its arguments. OR call func with fewer than it expects but returns a function so can call the rest
+// Currying: creates unary function that takes multiple arguments one at a time
+
+// Exercise 7
+
+const users = [{name: "James",score: 30,tries: 1}, {name: "Mary", score: 110,tries: 4}, {name: "Henry",score: 80,tries: 3}];
+
+//Modifies Data
+var storeUser = function(arr, user) {
+    return arr.map(function(val) {
+        if (val.name.toLowerCase() === user.name.toLowerCase()) {
+            return user;
+        } else {
+            return val;
+        }
+    });
+};
+
+//Pure Functions
+const cloneObj = function(obj) {
+    return JSON.parse(JSON.stringify(obj));
+};
+
+var getUser = function(arr, name) {
+    return arr.reduce(function(obj, val) {
+        if (val.name.toLowerCase() === name.toLowerCase()) {
+            return val;
+        } else {
+            return obj;
+        }
+    }, null);
+};
+
+var updateScore = function(user, newAmt) {
+    if (user) {
+        user.score += newAmt;
+        return user;
+    }
+};
+
+var updateTries = function(user) {
+    if (user) {
+        user.tries++;
+        return user;
+    }
+};
+
+const usr = getUser(users, "Henry");
+const usr1 = updateScore(cloneObj(usr), 30);
+const usr2 = updateTries(cloneObj(usr1));
+const newArray = storeUser(users, usr2);
+
+// Using currying and composition to create a specialized function that always acts on the users array but allows you to enter a user name. Have it return a clone of that user.
+const getUsersUser = pipe(
+    curry(getUser)(users),
+    cloneObj
+); 
+
+// Using your curried function, compose a new specialized function that will be used to update Henry. (Only invoked if you want to update Henry). It should accepts a new score and then return a new array that contains the updated score and tries. To compose this function you may need to create other functions.
+const updateHenry = pipe(
+    curry(updateScore)(getUsersUser('Henry')),
+    cloneObj,
+    updateTries,
+    curry(storeUser)(users)
+);
 
